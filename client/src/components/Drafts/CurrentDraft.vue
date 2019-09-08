cdefa6825b49ed342f39b543080e2090887163cf
DynamikArray <dynamikarray@gmail.com>
2019-09-08 08:52:31 -0400

    ADDED: title and publisher to current draft section

--- client/src/components/Drafts/CurrentDraft.vue
+++ client/src/components/Drafts/CurrentDraft.vue
@@ -1,22 +1,33 @@
 <template>
-  <v-card>
-    <v-card-text>
-      <p>
-        Title: <b v-if="selectedTitle">{{ selectedTitle.title }}</b>
-      </p>
-      <p>
-        Publisher: <b v-if="selectedTitle">{{ selectedTitle.publisher }}</b>
-      </p>
+  <div class="pa-3">
+    <v-card color="info">
+      <v-card-text>
+        <p>
+          Title: <b v-if="selectedTitle">{{ selectedTitle.title }}</b>
+        </p>
+        <p>
+          Publisher: <b v-if="selectedTitle">{{ selectedTitle.publisher }}</b>
+        </p>
 
-      <div v-if="draft.items">
-        <div v-for="item in draft.items">
-          <p>
-            {{ item.title }}
-          </p>
+        <div v-if="draft.items">
+          <div v-for="item in draft.items">
+            <p>
+              {{ item.title }}
+            </p>
+          </div>
         </div>
-      </div>
-    </v-card-text>
-  </v-card>
+      </v-card-text>
+    </v-card>
+
+    <v-divider class="my-3"></v-divider>
+
+    <div>Items go here</div>
+    <div>Items go here</div>
+    <div>Items go here</div>
+    <div>Items go here</div>
+    <div>Items go here</div>
+    <div>Items go here</div>
+  </div>
 </template>
 
 <script>
