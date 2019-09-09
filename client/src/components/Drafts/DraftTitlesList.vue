89f2a48350f371e2de45dbc4a0dabd6bf84bbc29
DynamikArray <dynamikarray@gmail.com>
2019-09-09 18:37:46 -0400

    ADDED: remove currentDrafts Titles button

--- client/src/components/Drafts/DraftTitlesList.vue
+++ client/src/components/Drafts/DraftTitlesList.vue
@@ -3,12 +3,13 @@
     <div v-if="titles.length" class="w-100">
       <div class="d-flex justify-space-between">
         <div class="d-flex flex grow align-center ml-2">
-          <h2>
-            {{ titles.length }}<span class="subtitle-1 mx-1">Titles</span>
-          </h2>
+          <h2>{{ titles.length }}<span class="subtitle-1">Titles</span></h2>
         </div>
         <div class="d-flex align-center mr-4">
-          <h5>Publisher</h5>
+          <h3 class="overline">Publisher</h3>
+        </div>
+        <div class="d-flex align-center mr-4">
+          <h5 class="overline">Remove</h5>
         </div>
       </div>
 
@@ -18,13 +19,18 @@
         <div
           v-for="title in titles"
           :key="title.titleId"
-          class="d-flex flex-row w-100 px-3 justify-space-between"
+          class="d-flex flex-row w-100 px-3 justify-space-between "
         >
-          <div class="d-flex grow">
+          <div class="d-flex grow align-center">
             <h4>{{ title.title }}</h4>
           </div>
-          <div class="d-flex">
+          <div class="d-flex align-center mr-3">
             <h4>{{ title.publisher }}</h4>
+          </div>
+          <div class="d-flex align-center ml-3">
+            <v-btn small text color="red" @click="removeDraftTitle(title)">
+              <v-icon small>fa-times-circle</v-icon>
+            </v-btn>
           </div>
         </div>
       </v-scroll-y-transition>
@@ -34,13 +40,18 @@
 
 <script>
 import { mapState } from "vuex";
-import {} from "@/store/mutation-types.js";
+import { CURRENT_DRAFT_TITLE_REMOVE } from "@/store/mutation-types.js";
 
 export default {
   computed: {
     ...mapState({
       titles: state => state.currentDraft.titles
     })
+  },
+  methods: {
+    removeDraftTitle(title) {
+      this.$store.commit(`currentDraft/${CURRENT_DRAFT_TITLE_REMOVE}`, title);
+    }
   }
 };
 </script>
