5f3b73b84d2eb125bb133ee03d5d673e9d1c3061
DynamikArray <dynamikarray@gmail.com>
2019-09-09 21:50:09 -0400

    FIXED: Incorrect port assignment.

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const config = {
   host: "localhost",
-  port: process.env.SERVER_PORT || 3000,
+  port: process.env.PORT || 3000,
   name: process.env.APP_NAME || "Api",
   logger: true
 };
