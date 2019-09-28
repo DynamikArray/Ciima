a72eba4ceb0b806d9063832f421975c211381b1d
DynamikArray <dynamikarray@gmail.com>
2019-09-27 23:11:55 -0400

    UPDATED: Autofill with better breakdown of methods, and enhanced to extend title.  Removed extra space, and fixes so the first book with data upc, dates, etc gets used.

--- client/src/components/Draft/fields/AutoFillButton.vue
+++ client/src/components/Draft/fields/AutoFillButton.vue
@@ -1,8 +1,22 @@
 <template>
-  <div class="d-flex justify-space-around w-100">
-    <div class="d-flex">
+  <div class="d-flex justify-center w-100">
+    <div class="d-flex align-center">
+      <h3 class="mx-1"><v-icon class="mr-1"></v-icon>Defaults:</h3>
+    </div>
+    <div class="d-flex mx-1 align-center mr-3">
       <v-btn :loading="draft.savingDraft" color="primary" @click="autofillDraft"
-        ><v-icon class="mr-2">fa-magic</v-icon>Auto Fill</v-btn
+        ><v-icon class="mr-2">fa-magic</v-icon>Autofill</v-btn
+      >
+    </div>
+    <div class="d-flex align-center ml-3">
+      <h3 class="mx-1"><v-icon class="mr-1"></v-icon>Enhanced Title:</h3>
+    </div>
+    <div class="d-flex mx-1 align-center">
+      <v-btn
+        :loading="draft.savingDraft"
+        color="primary"
+        @click="autoFillTitleWithPublisherYear"
+        ><v-icon class="mr-2">fab fa-leanpub</v-icon>Pub&Year</v-btn
       >
     </div>
   </div>
@@ -14,11 +28,9 @@ import { mapState } from "vuex";
 
 export default {
   data: () => ({
-    ebayStoreCategoryIdOne: 5837309015
+    ebayStoreCategoryIdOne: 5837309015 //catId for Sets
   }),
-  props: {
-    switchToTab: [Function]
-  },
+  props: {},
   computed: {
     ...mapState({
       draft: state => state.currentDraft,
@@ -27,69 +39,135 @@ export default {
     })
   },
   methods: {
+    getIssueNumbers() {
+      const issueNumbs = this.issues.map(issue => {
+        return issue.fullIssue;
+      });
+      return issueNumbs;
+    },
     //
     //
+    getPublisher() {
+      const { publisher } = this.titles.find(
+        titles => titles.publisher !== false
+      );
+      return publisher;
+    },
     //
-    autofillDraft() {
+    //
+    getCoverDate() {
+      const { coverDate } = this.issues.find(
+        issue => issue.coverDate !== false
+      );
+      return coverDate;
+    },
+    //
+    //
+    getUpc() {
+      const { upc } = this.issues.find(issue => issue.upc !== false);
+      return upc;
+    },
+    //
+    //
+    createTitleWithOptions(pub = false, year = false) {
+      const issueNumbers = this.getIssueNumbers();
+      const publisher = this.getPublisher();
+      const coverDate = this.getCoverDate();
+      //plceholder for values later
+      let titleYear = "";
+      if (coverDate) titleYear = format(coverDate, "YYYY");
+
+      const title = [];
+      //title
+      title.push(this.draft.titles[0].title);
+      //issue numbers
+      title.push(this.draft.issueNumbers);
+      //publisher
+      if (pub) title.push(publisher);
+      //year
+      if (year) title.push(titleYear);
+      //default verbiage
+      title.push("Complete Set Run Lot");
+      //issue first to last
+      title.push(`${issueNumbers.shift()}-${issueNumbers.pop()}`);
+      //grade
+      title.push(this.draft.grade);
+      //join it all up and ship it back
+      return title.join(" ");
+    },
+    //
+    //
+    runDataCheck() {
       if (!this.titles.length > 0) {
         this.$toastr.w("No Title(s) selected to get Comic data from.");
-        return;
+        return false;
       }
 
       if (!this.issues.length > 0) {
         this.$toastr.w("No Issue(s) selected to get Comic data from.");
-        return;
+        return false;
       }
+      return true;
+    },
+    //
+    //
+    autofillDraft() {
+      if (!this.runDataCheck()) return false;
+      //set 1st store cat
+      this.draft.ebayStoreCategoryIdOne = this.ebayStoreCategoryIdOne;
 
-      //issue numbers as array for use in method
-      const issuesNumbs = this.issues.map(issue => {
-        return issue.fullIssue;
-      });
-
-      //first last for title string
-      const issueFirstLast = {};
-      issueFirstLast.first = [...issuesNumbs].shift(); //leave original intact ;)
-      issueFirstLast.last = [...issuesNumbs].pop(); //leave original intact ;)
-
-      //issueNumbers list in draft
-      this.draft.issueNumbers = issuesNumbs.join(" ");
+      //issue Number
+      this.draft.issueNumbers = this.getIssueNumbers().join(" ");
 
       //publisher/year/date
-      this.draft.publisher = this.titles[0].publisher;
+      this.draft.publisher = this.getPublisher();
 
-      const coverDate = this.issues[0].coverDate;
+      //bring in upc from 1st issue
+      this.draft.upc = this.getUpc();
+
+      //cover date
+      const coverDate = this.getCoverDate();
       if (coverDate) {
         this.draft.publishedYear = format(coverDate, "YYYY");
         this.draft.publishedDate = format(coverDate, "MMM YYYY");
       }
 
-      //Fancy Title attempt
-      this.draft.inventoryTitle = `${this.draft.titles[0].title}  ${
-        this.draft.issueNumbers
-      } Complete Set Run Lot ${issueFirstLast.first}-${issueFirstLast.last} ${
-        this.draft.grade
-      }`;
-
-      //bring in upc from 1st issue
-      const { upc } = this.issues[0];
-      this.draft.upc = upc;
-
-      //set ebayCat from db list
-      //const { eBayCat1 } = this.issues[0];
-      //this.draft.ebaySiteCategoryId = eBayCat1;
-
-      //set 1st store cat
-      this.draft.ebayStoreCategoryIdOne = this.ebayStoreCategoryIdOne;
+      this.draft.inventoryTitle = this.createTitleWithOptions(false, false);
 
       //Tell user we did a thing
       this.$toastr.s(
-        "Form data Automagically&trade; filled in using selected title & issues!"
+        "Form data Automagically&trade; filled in using selected title & issues data!"
       );
+    },
+    autoFillTitleWithPublisherYear() {
+      if (!this.runDataCheck()) return false;
+      this.draft.inventoryTitle = this.createTitleWithOptions(true, true);
     }
     //
     //
   }
 };
+/*
+
+
+    //
+    //
+    createTitle() {
+      const issueNumbers = this.getIssueNumbers();
+
+      //Fancy Title attempt
+      const autoTitle = `${this.draft.titles[0].title} ${
+        this.draft.issueNumbers
+      } Complete Set Run Lot ${issueNumbers.shift()}-${issueNumbers.pop()} ${
+        this.draft.grade
+      }`;
+
+      return autoTitle;
+    },
+
+
+
+ */
 </script>
 
 <style scoped></style>
