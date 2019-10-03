e8f1cbf0e15b9e69a6b6c043a8d219c395d7a73d
Dynamik Array <dynamikarray@gmail.com>
2019-10-03 14:19:17 -0400

    ADDED: subheading text to CiiMa titles on homepage and login

--- client/src/views/Homepage.vue
+++ client/src/views/Homepage.vue
@@ -6,6 +6,7 @@
     <div class="d-flex" style="min-width:500px">
       <div class="about text-center w-100">
         <h1 class="display-4">CiiMA</h1>
+        <h4 class="">Comic Issues Inventory & Media Application</h4>
         <v-divider class="my-4"></v-divider>
 
         <div v-if="isLoggedIn">
