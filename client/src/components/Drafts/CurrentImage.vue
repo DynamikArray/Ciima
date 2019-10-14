ff38ff6ed434a92ea4c94737106e6a975b7fda8b
Dynamik Array <dynamikarray@gmail.com>
2019-10-14 14:55:55 -0400

    UPDATED: Buttons and Spacing size around draft image

--- client/src/components/Drafts/CurrentImage.vue
+++ client/src/components/Drafts/CurrentImage.vue
@@ -13,9 +13,9 @@
       </div>
     </div>
 
-    <div v-if="draft.coverPhoto" class="d-flex justify-center mt-6">
+    <div v-if="draft.coverPhoto" class="d-flex justify-center mt-1">
       <div v-if="draft.coverPhoto" class="d-flex flex-column">
-        <div class="d-flex justify-center my-3">
+        <div class="d-flex justify-center my-1">
           <div v-if="draft.savingCover">
             <v-icon color="blue darken-1" x-large
               >fas fa-circle-notch fa-spin</v-icon
@@ -25,39 +25,41 @@
             <v-img
               id="coverPhoto"
               :src="draft.coverPhoto"
-              width="240"
+              width="140"
               class="d-flex shrink"
             ></v-img>
           </div>
         </div>
 
-        <div class="d-flex justify-center my-3">
+        <div class="d-flex justify-center my-1">
           <div class="mx-4">
             <v-btn
+              x-small
               :disabled="draft.savingCover"
               v-if="draft.coverPhoto"
               success
               color="red"
               @click="deleteImagePhoto()"
             >
-              <v-icon class="mr-2">fa-times-circle</v-icon>Delete
+              <v-icon x-small class="mr-2">fa-times-circle</v-icon>Delete
             </v-btn>
           </div>
           <div class="mx-4">
             <v-btn
+              x-small
               :disabled="draft.savingCover"
               v-if="draft.coverPhoto"
               success
               color="warning"
               :to="'/images'"
             >
-              <v-icon class="mr-2">fa-image</v-icon>Edit
+              <v-icon x-small class="mr-2">fa-image</v-icon>Edit
             </v-btn>
           </div>
 
           <div class="mx-4">
-            <v-btn color="primary" @click="downloadImages"
-              ><v-icon class="mr-2">fa-download</v-icon>Download</v-btn
+            <v-btn x-small color="primary" @click="downloadImages"
+              ><v-icon x-small class="mr-2">fa-download</v-icon>Download</v-btn
             >
           </div>
         </div>
