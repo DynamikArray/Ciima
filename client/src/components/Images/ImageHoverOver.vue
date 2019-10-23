cbbf657e5571f81d3e21b7ee412a0535572adfd0
DynamikArray <dynamikarray@gmail.com>
2019-10-22 23:08:47 -0400

    FIXES: #35 thumbnails now change when rows updated

--- client/src/components/Images/ImageHoverOver.vue
+++ client/src/components/Images/ImageHoverOver.vue
@@ -1,18 +1,50 @@
 <template>
-  <v-tooltip color="black" :max-width="340" right>
-    <template v-slot:activator="{ on }">
-      <v-img :src="imageThumb" v-on="on" max-width="40" />
-    </template>
-    <v-img :src="imageFull" :max-width="340" contain> </v-img>
-  </v-tooltip>
+  <div>
+    <v-tooltip v-model="blnShow" color="black" :max-width="340" right>
+      <template v-slot:activator="{ on }">
+        <img
+          :key="makeFileNameKey()"
+          :src="getImageThumb()"
+          @mouseover="blnShow = true"
+          @mouseleave="blnShow = false"
+          :max-height="60"
+          contain
+        />
+      </template>
+
+      <v-img :src="imageFull" :max-width="340" contain>
+        <template v-slot:placeholder>
+          <v-row class="fill-height ma-0" align="center" justify="center">
+            <v-progress-circular
+              indeterminate
+              color="blue darken-1"
+            ></v-progress-circular>
+          </v-row>
+        </template>
+      </v-img>
+    </v-tooltip>
+  </div>
 </template>
 
 <script>
 /* */
 export default {
+  data: () => ({
+    blnShow: false
+  }),
   props: {
     imageFull: [String],
     imageThumb: [String]
+  },
+  methods: {
+    getImageThumb() {
+      return this.imageThumb;
+    },
+    makeFileNameKey() {
+      const thumb = this.imageThumb;
+      const path = thumb.split("/").reverse();
+      return path[1] || Date.now();
+    }
   }
 };
 </script>
