96818e69cdfff732fc2341f047c08de898ff4795
DynamikArray <DynamikArray@gmail.com>
2020-05-31 22:26:23 -0400

    FIXED: missing file

--- util/amqp/publishMessage.js
+++ util/amqp/publishMessage.js
@@ -1,21 +1,21 @@
-const amqpWrapper = require("./amqpLib.js");
+const amqpWrapper = require("./amqplib.js");
 
 /**
  * [publishMessage description]
  * @param  {[type]} payload [description]
  * @return {[type]}         [description]
  */
-const publishMessage = payload => {
+const publishMessage = (payload) => {
   // Publisher
-  amqpWrapper.CONNECTION.then(function(conn) {
+  amqpWrapper.CONNECTION.then(function (conn) {
     return conn.createChannel();
   })
-    .then(function(ch) {
-      return ch.assertQueue(amqpWrapper.QUEUE_NAME).then(function(ok) {
+    .then(function (ch) {
+      return ch.assertQueue(amqpWrapper.QUEUE_NAME).then(function (ok) {
         return ch.sendToQueue(amqpWrapper.QUEUE_NAME, Buffer.from(payload));
       });
     })
-    .catch(e => {
+    .catch((e) => {
       linnworks.logger.error(e);
     });
 };
