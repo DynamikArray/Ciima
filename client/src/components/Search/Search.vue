36ddb41bbcf62e545aeb01e1b2de46633d797e5e
DynamikArray <DynamikArray@gmail.com>
2020-07-02 06:19:10 -0400

    FIXED: UPC scanning to work along side regular inputting

--- client/src/components/Search/Search.vue
+++ client/src/components/Search/Search.vue
@@ -2,7 +2,19 @@
   <section>
     <v-card raised outlined>
       <v-card-text class="">
-        <v-form v-model="validTitleSearch" @submit.prevent="searchTitles">
+        <v-form v-model="validTitleSearch" @submit.prevent="handleSearchInput">
+          <v-text-field
+            autocomplete="off"
+            autofocus
+            hide-details
+            persistent-hint
+            v-model="searchTitleString"
+            label="Search Titles"
+            prepend-icon="fa-search"
+            @input="handleSearchInput"
+          />
+
+          <!--
           <v-text-field
             autofocus
             hide-details
@@ -24,8 +36,10 @@
               </v-btn>
             </template>
           </v-text-field>
+        -->
         </v-form>
 
+        <!--
         <v-form
           v-if="showAdvanced"
           v-model="validIssueSearch"
@@ -41,7 +55,9 @@
             v-on:keyup.enter="searchIssues"
           ></v-text-field>
         </v-form>
+        -->
 
+        <!--
         <v-form
           v-if="showUpc"
           v-model="validUpcSearch"
@@ -57,6 +73,7 @@
             v-on:keyup.enter="searchUpc"
           ></v-text-field>
         </v-form>
+        -->
       </v-card-text>
     </v-card>
   </section>
@@ -72,19 +89,60 @@ import {
 
 export default {
   data: () => ({
-    searchUpcString: "",
-    validUpcSearch: false,
+    //searchUpcString: "",
+    //validUpcSearch: false,
 
     searchTitleString: "",
-    validTitleSearch: false,
+    validTitleSearch: false
 
-    searchIssuesString: "",
-    validIssueSearch: false,
+    //searchIssuesString: "",
+    //validIssueSearch: false,
 
-    showAdvanced: false,
-    showUpc: false
+    //showAdvanced: false,
+    //showUpc: false
   }),
   methods: {
+    handleSearchInput: debounce(function(event) {
+      //search value
+      const search = this.searchTitleString;
+      //on submit or debounce after 3 chars
+      if (event.type === "submit" || search.length > 3) {
+        //when submit is prssed make sure its not empty
+        if (!search.length == 0) {
+          if (!Number.isNaN(Number(search))) {
+            //is numeric should be upc
+            if (search.length == 17) {
+              this.fetchTitlesByUpc(search);
+            } else {
+              this.$toastr.e(
+                `<b>Invalid UPC detected:</b><br /><b>${search}</b><br/>
+                Not enough characters, try rescanning.`
+              );
+              this.searchTitleString = "";
+            }
+          } else {
+            this.fetchTitlesByString(search);
+          }
+        }
+      }
+    }, 500),
+    fetchTitlesByString(search) {
+      this.$store.dispatch(`titleSearch/${SEARCH_TITLES}`, {
+        query: search
+      });
+    },
+    fetchTitlesByUpc(search) {
+      this.$store
+        .dispatch(`titleSearch/${SEARCH_UPC}`, {
+          query: search,
+          upc: 1
+        })
+        .then(res => {
+          this.$router.push({ name: "issues" });
+        });
+    }
+
+    /*
     searchTitles: debounce(function(event) {
       const search = this.searchTitleString;
 
@@ -98,8 +156,10 @@ export default {
         }
       }
     }, 500),
+    */
     //
     //
+    /*
     searchIssues: debounce(function(event) {
       const search = this.searchIssuesString;
 
@@ -113,7 +173,8 @@ export default {
         }
       }
     }, 500),
-
+    */
+    /*
     searchUpc: debounce(function(event) {
       const search = this.searchUpcString;
 
@@ -131,7 +192,8 @@ export default {
         }
       }
     }, 500),
-
+    */
+    /*
     toggleAdvancedSearch() {
       this.showAdvanced = !this.showAdvanced;
     },
@@ -147,6 +209,7 @@ export default {
       if (this.showUpc) return "fa-arrow-alt-circle-up";
       return "fa-arrow-alt-circle-down";
     }
+    */
   }
 };
 </script>
