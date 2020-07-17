f975639fbb2126f7e40368dd9a8b14afe875e030
DynamikArray <DynamikArray@gmail.com>
2020-07-16 20:55:41 -0400

    UPDATED Navigation scroller to use vuescrol

--- client/src/components/layout/navigation/NavigationDrawer.vue
+++ client/src/components/layout/navigation/NavigationDrawer.vue
@@ -2,82 +2,95 @@
   <v-navigation-drawer
     ref="mainNavigationDrawer"
     :miniVariant="compDrawer"
-    mini-variant-width="50"
+    mini-variant-width="54"
     app
     clipped
     permanent
   >
-    <div class="d-flex flex-column">
-      <div class="d-flex justify-start flex-column">
-        <div v-if="!compDrawer">
-          <h3 class="text-center pl-1 pt-2">Main Menu</h3>
-        </div>
+    <vuescroll :ops="ops" class="">
+      <div class="HideOverflowY mb-0 pr-2">
+        <div class="d-flex flex-column">
+          <div class="d-flex justify-start flex-column">
+            <div v-if="!compDrawer">
+              <h3 class="text-center pl-1 pt-2">Main Menu</h3>
+            </div>
 
-        <div class="d-flex">
-          <v-divider v-if="!compDrawer" class="my-2"></v-divider>
-        </div>
+            <div class="d-flex">
+              <v-divider v-if="!compDrawer" class="my-2"></v-divider>
+            </div>
 
-        <v-list v-if="loggedIn" dense class="py-0">
-          <div v-for="link in links">
-            <router-link
-              v-if="!link.menuDivider"
-              :key="link.text"
-              :to="{ name: link.to }"
-              class="noUnderline"
-            >
-              <v-list-item @click="" v-ripple>
-                <v-list-item-action :class="removeMargin()"
-                  ><v-icon>{{ link.icon }}</v-icon>
-                </v-list-item-action>
+            <v-list v-if="loggedIn" dense class="py-0">
+              <div v-for="link in links">
+                <router-link
+                  v-if="!link.menuDivider"
+                  :key="link.text"
+                  :to="{ name: link.to }"
+                  class="noUnderline"
+                >
+                  <v-list-item @click="" v-ripple>
+                    <v-list-item-action :class="removeMargin()"
+                      ><v-icon>{{ link.icon }}</v-icon>
+                    </v-list-item-action>
 
-                <v-list-item-content v-if="!compDrawer">{{
-                  link.text
-                }}</v-list-item-content>
-              </v-list-item>
-            </router-link>
-            <v-divider v-if="link.menuDivider" class="my-1"></v-divider>
-          </div>
-        </v-list>
+                    <v-list-item-content v-if="!compDrawer">{{
+                      link.text
+                    }}</v-list-item-content>
+                  </v-list-item>
+                </router-link>
+                <v-divider v-if="link.menuDivider" class="my-1"></v-divider>
+              </div>
+            </v-list>
 
-        <v-list v-else>
-          <v-list-item v-if="!compDrawer" class="text-center pa-2 mx-2 mb-3">
-            <v-list-item-content v-if="!compDrawer">
-              <h4>You must be signed in to continue using the application.</h4>
-            </v-list-item-content>
-          </v-list-item>
-
-          <v-divider v-if="!compDrawer" class="my-1"></v-divider>
-          <router-link to="login" class="noUnderline">
-            <v-list-item @click="" v-ripple>
-              <v-list-item-action :class="removeMargin()"
-                ><v-icon>fa-sign-in-alt</v-icon>
-              </v-list-item-action>
-
-              <v-list-item-content v-if="!compDrawer"
-                >Sign In</v-list-item-content
+            <v-list v-else>
+              <v-list-item
+                v-if="!compDrawer"
+                class="text-center pa-2 mx-2 mb-3"
               >
-            </v-list-item>
-          </router-link>
-        </v-list>
-      </div>
+                <v-list-item-content v-if="!compDrawer">
+                  <h4>
+                    You must be signed in to continue using the application.
+                  </h4>
+                </v-list-item-content>
+              </v-list-item>
 
-      <div class="d-flex">
-        <v-divider v-if="!compDrawer" class="my-2"></v-divider>
-      </div>
+              <v-divider v-if="!compDrawer" class="my-1"></v-divider>
+              <router-link to="login" class="noUnderline">
+                <v-list-item @click="" v-ripple>
+                  <v-list-item-action :class="removeMargin()"
+                    ><v-icon>fa-sign-in-alt</v-icon>
+                  </v-list-item-action>
 
-      <div v-if="!compDrawer">
-        <Usercard v-if="!compDrawer" class="my-2" />
+                  <v-list-item-content v-if="!compDrawer"
+                    >Sign In</v-list-item-content
+                  >
+                </v-list-item>
+              </router-link>
+            </v-list>
+          </div>
+
+          <div class="d-flex">
+            <v-divider v-if="!compDrawer" class="my-2"></v-divider>
+          </div>
+
+          <div v-if="!compDrawer">
+            <Usercard v-if="!compDrawer" class="my-2" />
+          </div>
+        </div>
       </div>
-    </div>
+    </vuescroll>
   </v-navigation-drawer>
 </template>
 
 <script>
+import vuescroll from "vuescroll";
+import { scrollbarSettings } from "@/util/scrollbarSettings";
+
 import Links from "./_navigationLinks";
 import Usercard from "@/components/layout/user/Usercard";
 export default {
   components: {
-    Usercard
+    Usercard,
+    vuescroll
   },
   props: {
     drawer: [Boolean],
@@ -88,9 +101,15 @@ export default {
       this.compDrawer = val;
     }
   },
-  data: () => ({
-    compDrawer: true
-  }),
+  data: () => {
+    scrollbarSettings.bar.keepShow = false;
+    scrollbarSettings.bar.onlyShowBarOnScroll = true;
+    scrollbarSettings.rail.keepShow = false;
+    return {
+      compDrawer: true,
+      ops: scrollbarSettings
+    };
+  },
   created() {
     this.links = Links;
   },
