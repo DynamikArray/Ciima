f6476da03d82a40088cb42701046fb6ca662fd4c
DynamikArray <DynamikArray@gmail.com>
2021-02-20 21:48:53 -0500

    Added Clone Draft for GTC's.

--- client/src/store/gtcs/draft/draft.js
+++ client/src/store/gtcs/draft/draft.js
@@ -4,15 +4,19 @@ Vue.use(Vuex);
 
 import { getField, updateField } from "vuex-map-fields";
 
-import { CURRENT_GTC_DRAFT_SAVE } from "@/store/action-types";
+import { CURRENT_GTC_DRAFT_SAVE, NEW_GTC_DRAFT_LOAD } from "@/store/action-types";
 
 import {
   RESET_GTC_DRAFT,
   UPDATE_GTC_DRAFT_IMAGES,
   GTC_SET_IMAGE_TO_CROP,
   CURRENT_GTC_DRAFT_UPDATE,
-  CURRENT_GTC_DRAFT_SAVING
+  CURRENT_GTC_DRAFT_SAVING,
+  CURRENT_GTC_DRAFT_LOAD,
+  SET_DEFAULT_PRODUCT_TYPE
 } from "@/store/mutation-types";
+
+import router from "@/router/router.js";
 
 const defaultDraft = {
   inventoryTitle: "",
@@ -34,7 +38,9 @@ const defaultDraft = {
   images: [],
   imageToCrop: false,
   savingGtcDraft: false,
-  savingGtcDraftMessage: false
+  savingGtcDraftMessage: false,
+
+  isCloned: false
 };
 
 const draft = {
@@ -47,7 +53,6 @@ const draft = {
   },
   mutations: {
     updateField,
-
     [CURRENT_GTC_DRAFT_UPDATE](state, draft) {
       state = { ...state, ...draft };
     },
@@ -56,6 +61,7 @@ const draft = {
       state.savingGtcDraftMessage = data.loadingMessage || "Saving ...";
     },
     [RESET_GTC_DRAFT](state) {
+      state.isCloned = defaultDraft.isCloned;
       state.locationCode = defaultDraft.locationCode;
       state.inventoryTitle = defaultDraft.inventoryTitle;
 
@@ -83,6 +89,30 @@ const draft = {
     },
     [GTC_SET_IMAGE_TO_CROP](state, image) {
       state.imageToCrop = image;
+    },
+    [CURRENT_GTC_DRAFT_LOAD](state, item) {
+      state.isCloned = true;
+      state.locationCode = defaultDraft.locationCode;
+      state.inventoryTitle = item.inventoryTitle;
+
+      state.price = item.price.toString();
+      state.declinePrice = item.declinePrice.toString();
+
+      state.quantity = item.quantity.toString();
+
+      state.mainCharacter = item.mainCharacter;
+      state.publisher = item.publisher;
+      state.extraDescription = item.extraDescription;
+
+      state.images = defaultDraft.images;
+      state.imageToCrop = defaultDraft.imageToCrop;
+
+      state.ebaySiteCategoryId = item.ebaySiteCategoryId;
+      state.ebayStoreCategoryIdOne = item.ebayStoreCategoryIdOne;
+      state.ebayStoreCategoryIdTwo = item.ebayStoreCategoryIdTwo;
+
+      state.savingGtcDraft = defaultDraft.savingGtcDraft;
+      state.savingGtcDraftMessage = defaultDraft.savingGtcDraftMessage;
     }
   },
   actions: {
@@ -103,6 +133,12 @@ const draft = {
       } catch (err) {
         throw err;
       }
+    },
+    async [NEW_GTC_DRAFT_LOAD]({ dispatch, commit }, item) {
+      commit(`${RESET_GTC_DRAFT}`);
+      commit(`${CURRENT_GTC_DRAFT_LOAD}`, item);
+      commit(`settings/${SET_DEFAULT_PRODUCT_TYPE}`, "gtc", { root: true });
+      router.push({ name: "draft" });
     }
   }
 };
