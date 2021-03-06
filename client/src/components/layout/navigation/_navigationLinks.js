64ae715c0274155ed8ae986071f5e34fd8f2b064
DynamikArray <DynamikArray@gmail.com>
2021-03-05 23:35:59 -0500

    Updated navigation link Icon for reports

--- client/src/components/layout/navigation/_navigationLinks.js
+++ client/src/components/layout/navigation/_navigationLinks.js
@@ -12,7 +12,7 @@ module.exports = [
   { menuDivider: true },
   { icon: "fa fa-chart-bar", text: "Analytics", to: "analytics" },
   { menuDivider: true },
-  { icon: "fa fa-file-invoice-dollar", text: "Sold Items Report", to: "sold.items.report" },
+  { icon: "fa fa-file-invoice", text: "Sold Items Report", to: "sold.items.report" },
   { icon: "fa fa-file-invoice-dollar", text: "Category Sales Report", to: "category.sales.report" },
   { menuDivider: true },
   { icon: "fa fab fa-tools", text: "Tools & Utilities", to: "tools" }
