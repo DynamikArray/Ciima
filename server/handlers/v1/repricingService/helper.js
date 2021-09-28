b3ab342e5a13ff47054ce016ff0cc9bf4511568f
DynamikArray <dynamikarray@gmail.com>
2021-09-27 21:57:14 -0400

    Fix Repricing Log returned date time stamps

--- server/handlers/v1/repricingService/helper.js
+++ server/handlers/v1/repricingService/helper.js
@@ -1,5 +1,5 @@
 const buildSelectQueries = () => {
-  const selectQuery = `SELECT * FROM slc_repricing_log ORDER BY ID DESC LIMIT ? OFFSET ?;`;
+  const selectQuery = `SELECT id, itemTitle, newPrice, oldPrice, pkStockItemID, CONVERT_TZ(dateUpdated,'+00:00','-04:00')as dateUpdated FROM slc_repricing_log ORDER BY ID DESC LIMIT ? OFFSET ?;`;
 
   const totalQuery = `SELECT COUNT(*)as rowCount FROM slc_repricing_log ORDER BY ID DESC;`;
 
